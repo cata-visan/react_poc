e7ef0fe334429e6434d860465689a76f12c05bb7
Cata Visan <rc.visan@gmail.com>
2022-04-20 22:27:17 +0300

    First Commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import ExpenseItem from './pages/poc/components/ExpenseItem';
+
+function App() {
+  return (
+   <div>
+     <h1>Learn React</h1>
+     <ExpenseItem />
+    </div>
+  )    
+}
+
+export default App;
---
+++ src/pages/poc/components/ExpenseItem.tsx
@@ -0,0 +1,13 @@
+function ExpenseItem() {
+  return (
+    <div>
+      <div>Today</div>
+      <div>
+        <h2>Ferobol Insurance</h2>
+        <div>800 de lei mai scump ca anul trecut fml</div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
